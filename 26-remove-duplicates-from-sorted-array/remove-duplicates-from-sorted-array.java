d776a6dc95837bf177f7cb4044dff5fa4e0df077
dhanuroo11-blip <dhanuroo11@gmail.com>
2025-10-12 18:39:10 +0530

    Time: 1 ms (57.77%) | Memory: 45 MB (42.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -10,13 +10,5 @@ class Solution {
         }
         return j;
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner (System.in);
-        int size=sc.nextInt();
-        int nums[]=new int[size];
-        for(int i=0;i<size;i++){
-            nums[i]=sc.nextInt();
-        }
-        System.out.println(removeDuplicates(nums));
-    }
+   
 }
