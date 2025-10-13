991c585690548150fb34338a22783ff4a6791942
dhanuroo11-blip <dhanuroo11@gmail.com>
2025-10-13 16:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (41.75%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int singlesum=0;
+        int doublesum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+                singlesum+=nums[i];
+            }else{
+                doublesum+=nums[i];
+            }
+        }
+        return singlesum!=doublesum;
+    }
+}
