563e35280a02bb8590027954b386458f819fe04f
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-04-20 17:20:30 +0530

    Time: 2 ms (91.44%) | Memory: 47.9 MB (23.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static void moveZeroes(int[] nums) {
+    public void moveZeroes(int[] nums) {
         int j=0;
        for(int i=0;i<nums.length;i++){
         if(nums[i]!=0){
@@ -9,16 +9,6 @@ class Solution {
            nums[j]=temp;
            j++;
         }
-       }
-       //return nums; 
-    }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int nums[]=new int[size];
-        for(int i=0;i<size;i++){
-            nums[i]=sc.nextInt();
-        }
-        System.out.println(nums);
+      }
     }
 }
