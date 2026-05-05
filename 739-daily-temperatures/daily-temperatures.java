dae4c8289c192e2e4380b9d31cc298dbb4923808
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-05-05 10:09:04 +0530

    Time: 61 ms (69.65%) | Memory: 107.5 MB (44.74%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // resolve previous colder days
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prev = stack.pop();
+                ans[prev] = i - prev;
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
