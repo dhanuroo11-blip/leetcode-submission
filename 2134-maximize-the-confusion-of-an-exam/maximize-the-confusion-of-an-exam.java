0a933a4a76ab3f17e3ab9b5ac2535c89eb6e63c3
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-06-29 10:08:50 +0530

    Time: 11 ms (95.96%) | Memory: 46.2 MB (81.02%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int n=answerKey.length();
+        int countf=0;
+        int countt=0;
+        int left=0;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            if(answerKey.charAt(i)=='T') countt++;
+            else countf++;
+            while(i-left+1-countt>k&&i-left+1-countf>k){
+                if(answerKey.charAt(left)=='T') countt--;
+                else countf--;
+                left++;
+            }
+            maxlen=Math.max(maxlen,i-left+1);
+        }
+        return maxlen;
+    }
+}
