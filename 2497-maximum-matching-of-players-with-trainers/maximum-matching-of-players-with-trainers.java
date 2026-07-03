7a459114cf408351435333314edb2627724614a2
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-07-03 14:35:54 +0530

    Time: 35 ms (5.30%) | Memory: 91 MB (12.91%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int ans=0;
+        int i=0,j=0;
+       while(i<players.length&&j<trainers.length){
+        if(players[i]<=trainers[j]){
+            ans++;
+            i++;
+        }
+        j++;
+       }
+        return ans;
+    }
+}
