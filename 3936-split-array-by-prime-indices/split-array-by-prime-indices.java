6e52540455218ef09db7d09d04460975bb743e82
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-07-08 14:50:42 +0530

    Time: 46 ms (40.11%) | Memory: 110.3 MB (26.37%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
@@ -4,18 +4,18 @@ class Solution {
         long sumb=0;
         
         for(int i=0;i<nums.length;i++){
-boolean found=true;
-if(i<=1){
-    found=false;
-}else{
+        boolean found=true;
+            if(i<=1){
+            found=false;
+        }
+        else{
             for(int j=2;j*j<=i;j++){
                 if(i%j==0){
                     found=false;
                     break;
                 }
-               // else found=false;
             }
-}
+        }
             if(found) suma+=nums[i];
             else sumb+=nums[i];
         }
