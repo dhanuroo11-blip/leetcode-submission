2aa9bff5b9ca182d46acd1208eb0b0ba68e151e1
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-07-09 10:54:57 +0530

    Time: 4 ms (99.45%) | Memory: 105.7 MB (80.22%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.java
@@ -1,24 +1,32 @@
 class Solution {
-    public long splitArray(int[] nums) {
+   
+        private static boolean[] s(int limit){
+            boolean arr[]=new boolean[limit+1];
+            Arrays.fill(arr,true);
+            arr[0]=false;
+            arr[1]=false;
+            for(int i=2;i*i<limit;i++){
+                 if(arr[i]){
+                    for(int j=i*i;j<limit;j+=i){
+                        arr[j]=false;
+                    }
+                 }
+            }
+            return arr;
+        }
+         public long splitArray(int[] nums) {
+        int max=0;
         long suma=0;
         long sumb=0;
         
+        boolean prime[]=s(nums.length);
         for(int i=0;i<nums.length;i++){
-        boolean found=true;
-            if(i<=1){
-            found=false;
+            if(prime[i]){
+                suma+=nums[i];
+            }else sumb+=nums[i];
         }
-        else{
-            for(int j=2;j*j<=i;j++){
-                if(i%j==0){
-                    found=false;
-                    break;
-                }
-            }
-        }
-            if(found) suma+=nums[i];
-            else sumb+=nums[i];
-        }
-        return Math.abs(suma-sumb);
+         return Math.abs(sumb-suma);
     }
 }
+    
+         
