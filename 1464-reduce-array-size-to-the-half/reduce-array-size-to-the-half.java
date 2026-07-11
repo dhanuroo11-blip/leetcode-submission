d859479401b3113099b23fa217961f5f8371fab6
dhanuroo11-blip <dhanuroo11@gmail.com>
2026-07-11 12:16:14 +0530

    Time: 27 ms (94.03%) | Memory: 88.9 MB (30.02%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        List<Integer> list=new ArrayList<>(map.values());
+        /*List<Integer> list=new ArrayList<>(map.values());
         list.sort(Collections.reverseOrder());
         int sum=0;
         int tar=arr.length/2;
@@ -12,9 +12,21 @@ class Solution {
         for(int i=0;i<list.size();i++){
             sum+=list.get(i);
               count++;
-             if(sum>=tar) break;
-          
-           
+             if(sum>=tar) break;     
+        }*/
+        int bucket[]=new int[arr.length+1];
+        for(int freq:map.values()){
+            bucket[freq]++;
+        }
+        int sum=0;
+        int count=0;
+        for(int i=arr.length;i>=1;i--){
+            while(bucket[i]>0){
+                sum+=i;
+                count++;
+                bucket[i]--;
+                if(sum>=arr.length/2) return count;
+            }
         }
         return count;
     }
